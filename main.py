790f9bed6068be1ee1ba481fb656c1a5fd9e47da
art-me-1101 <krutov.artm@yandex.ru>
2023-11-22 21:17:42 +0300

    lol2

--- main.py
+++ main.py
@@ -3,18 +3,15 @@ import random
 
 from PyQt5.QtCore import QPoint
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5 import uic
+from UI import Ui_MainWindow
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
 
 
-class MyWidget(QMainWindow):
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.do_paint = False
-        uic.loadUi('UI.ui', self)
-        self.setupUi()
-
-    def setupUi(self):
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.new_round)
 
     def new_round(self):
@@ -30,12 +27,13 @@ class MyWidget(QMainWindow):
         self.do_paint = False
 
     def draw_round(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
-        qp.setPen(QColor(255, 255, 0))
         for i in range(7):
-            a = random.randint(1, 100)
-            x = random.randint(0, self.size().width())
-            y = random.randint(0, self.size().height())
+            c = (random.randint(1, 255) for _ in range(3))
+            qp.setBrush(QColor(*c))
+            qp.setPen(QColor(*c))
+            a = random.randint(30, 300)
+            x = random.randint(0, self.size().width() - a)
+            y = random.randint(0, self.size().height() - a)
             qp.drawEllipse(x, y, a, a)
 
 
